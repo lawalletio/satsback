0c96cc9dc6067d71f7784be05b816ddc0aea7385
Rapax <gasparpelayo44@gmail.com>
2024-09-14 03:03:44 -0300

    fix: use nostr-tools

--- src/index.ts
+++ src/index.ts
@@ -1,127 +1,63 @@
 import 'websocket-polyfill';
 import * as dotenv from 'dotenv';
-import { makeEvent } from './utils/nostrEvent';
-import { ndkClient } from './services/ndk';
-import {
-    NDKEvent,
-    NDKFilter,
-    NDKKind,
-    NDKSubscription,
-    NDKSubscriptionOptions,
-} from '@nostr-dev-kit/ndk';
+import { Filter } from 'nostr-tools';
+import { sendCashBack } from './utils/sendCashBack';
+import { generateRelay } from './services/relay';
+import { SubscriptionParams } from './types/relay';
 
 dotenv.config();
 
 const start = async () => {
     try {
-        // NDK
-        await ndkClient.init();
-
-        const ndk = ndkClient.getInstance();
-
-        // Subscribe to events
+        // Keys
+        const privateKey = Uint8Array.from(
+            Buffer.from(process.env.MODULE_PRIVATE_KEY!, 'hex')
+        );
         const laposPublicKey = process.env.LAPOS_PUBLIC_KEY!;
         const ledgerPublicKey = process.env.LEDGER_PUBLIC_KEY!;
 
-        const subscriptionFilter: NDKFilter = {
-            kinds: [1112 as NDKKind],
-            authors: [ledgerPublicKey],
-            '#p': [laposPublicKey],
-            '#t': ['internal-transaction-ok'],
-            limit: 0,
-        };
+        // Relay
+        const relayUrl = 'wss://relay.lawallet.ar';
 
-        const subscriptionOptions: NDKSubscriptionOptions = {
-            closeOnEose: false,
-        };
+        // Filters
+        const filters: Filter[] = [
+            {
+                kinds: [1112],
+                authors: [ledgerPublicKey],
+                '#p': [laposPublicKey],
+                '#t': ['internal-transaction-ok'],
+                limit: 0,
+            },
+        ];
 
-        const subscription: NDKSubscription = ndk.subscribe(
-            subscriptionFilter,
-            subscriptionOptions,
-            ndk.devWriteRelaySet,
-            true
-        );
+        // Subscription
+        const subscriptionParams: SubscriptionParams = {
+            filters,
+            callback: async (event) => {
+                try {
+                    await sendCashBack(event, ledgerPublicKey, privateKey);
 
-        subscription.on('event', async (event: NDKEvent) => {
-            try {
-                console.log('Event received');
-
-                const eventTags = event.tags;
-                for (const tag in eventTags) {
-                    const userPublicKey =
-                        tag === '0' ? eventTags[tag][1] : null;
-
-                    // To Do: validate that the user is of lawallet
-
-                    const amount = JSON.parse(event.content).tokens.BTC;
-
-                    await sendCashBack(
-                        userPublicKey!,
-                        amount,
-                        ledgerPublicKey,
-                        ndk
-                    );
-
-                    break;
+                    // eslint-disable-next-line
+                } catch (error: any) {
+                    if (
+                        error.message === 'User not allowed to make cash back'
+                    ) {
+                        console.warn(
+                            'Unauthorized user tried to make cash back'
+                        );
+                    } else {
+                        console.error('Error in relay callback:', error);
+                    }
                 }
-
-                // eslint-disable-next-line
-            } catch (error: any) {
-                console.error('Error processing event in subscription:', error);
-            }
-        });
-
-        subscription.on('close', () => {
-            console.log('Subscription closed');
-
-            subscription.start();
-
-            console.log('Subscription started');
-        });
-    } catch {
-        console.error('Error general');
-    }
-};
-
-const sendCashBack = async (
-    userPublicKey: string,
-    amount: number,
-    ledgerPublicKey: string,
-    // eslint-disable-next-line
-    ndk: any
-) => {
-    try {
-        // Make event
-        const event = await makeEvent(
-            userPublicKey,
-            amount,
-            ledgerPublicKey,
-            ndk
-        );
-
-        // Options for fetch
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(event),
+            },
         };
 
-        // Publish event to relay.lawallet.ar
-        const response = await fetch(
-            'https://api.lawallet.ar/nostr/publish/',
-            options
-        );
+        const relay = await generateRelay(relayUrl, subscriptionParams);
 
-        if (!response.ok) {
-            throw new Error(`Failed to send cash back: ${response.statusText}`);
-        }
-
-        console.log('Cash back sent');
-
+        console.log('Relay successfully connected:', relay);
         // eslint-disable-next-line
     } catch (error: any) {
-        console.error('Error in sendCashBack:', error);
-        throw error;
+        console.error('Critical Error in start():', error);
     }
 };
 
