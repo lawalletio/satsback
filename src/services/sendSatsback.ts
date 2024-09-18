cb228bbd13ac8fa1fceeb98330a14f6b3101ba33
Rapax <gasparpelayo44@gmail.com>
2024-09-18 15:50:40 -0300

    fix: move sendSatsback to services

---
+++ src/services/sendSatsback.ts
@@ -0,0 +1,56 @@
+import { NostrEvent } from 'nostr-tools';
+import { makeEvent } from '../utils/makeEvent';
+
+async function sendSatsback(
+    event: NostrEvent,
+    ledgerPublicKey: string,
+    privateKey: Uint8Array
+) {
+    try {
+        // User public key
+        const eventTags = event.tags;
+        let userPublicKey: string;
+
+        for (const tag in eventTags) {
+            if (tag === '0') userPublicKey = eventTags[tag][1];
+        }
+
+        // Amount
+        const amount = JSON.parse(event.content).tokens.BTC;
+
+        // Make event
+        const eventToSent: NostrEvent = await makeEvent(
+            amount,
+            userPublicKey!,
+            ledgerPublicKey,
+            privateKey
+        );
+
+        // Options for fetch
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(eventToSent),
+        };
+
+        // Publish event to relay.lawallet.ar
+        const response = await fetch(
+            'https://api.lawallet.ar/nostr/publish/',
+            options
+        );
+
+        // Error
+        if (!response.ok) {
+            throw new Error(`Failed to send satsback: ${response.statusText}`);
+        }
+
+        console.log('Satsback sent');
+
+        // eslint-disable-next-line
+    } catch (error: any) {
+        console.error('Error in sendSatsback:', error);
+        throw error;
+    }
+}
+
+export { sendSatsback };
