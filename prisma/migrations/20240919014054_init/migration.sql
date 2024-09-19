fd2788905280a8b70468f4ba2ac66b04ce2017be
Rapax <gasparpelayo44@gmail.com>
2024-09-19 02:01:50 -0300

    fix: update migration

--- prisma/migrations/20240919014054_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `voucher` on the `Volunteer` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Volunteer" DROP COLUMN "voucher",
-ADD COLUMN     "voucher" INTEGER NOT NULL;
