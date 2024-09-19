c39ebd1abff594b01fc2e03b2f39719139955b63
Rapax <gasparpelayo44@gmail.com>
2024-09-19 17:50:40 -0300

    feat: add metadata to satsback event

--- src/utils/makeEvent.ts
+++ src/utils/makeEvent.ts
@@ -1,4 +1,10 @@
-import { EventTemplate, finalizeEvent, NostrEvent } from 'nostr-tools';
+import {
+    EventTemplate,
+    finalizeEvent,
+    getPublicKey,
+    nip04,
+    NostrEvent,
+} from 'nostr-tools';
 import * as dotenv from 'dotenv';
 import { prisma } from './prismaClient';
 import { Volunteer } from '@prisma/client';
@@ -11,6 +17,7 @@ const whitelistPublicKeys = {
     juan: '3699cf3fab1aa22dad84155639d35911013c63bbe6e26818e2584ed12cebeb6e',
     rapax: '994d52a31d3efd0ac661b1940e3f3dcae49c750d6dd90c68600589f272e3bc85',
     dios: 'cee287bb0990a8ecbd1dee7ee7f938200908a5c8aa804b3bdeaed88effb55547',
+    el7: 'ff64654a88bee78dba2d1d9999b4224fb72d4821e32e722ae3871f438431021b',
 };
 
 const whitelistVolunteers = {
@@ -88,6 +95,34 @@ async function makeEvent(
             satsbackAmount = roundAmount;
         }
 
+        // Metadata tag
+        // Sender
+        const senderPubkeyInfo = await fetch(
+            'https://lawallet.ar/api/pubkey/' + getPublicKey(privateKey)
+        );
+        const senderJson = await senderPubkeyInfo.json();
+        const senderWalias =
+            senderJson.username + '@' + senderJson.federationId;
+
+        // Receiver
+        const receiverPubkeyInfo = await fetch(
+            'https://lawallet.ar/api/pubkey/' + userPubkey
+        );
+        const receiverJson = await receiverPubkeyInfo.json();
+        const receiverWalias =
+            receiverJson.username + '@' + receiverJson.federationId;
+
+        const metadataContent: { sender: string; receiver: string } = {
+            sender: senderWalias,
+            receiver: receiverWalias,
+        };
+
+        const metadataContentEncrypt = await nip04.encrypt(
+            privateKey,
+            userPubkey,
+            JSON.stringify(metadataContent)
+        );
+
         // Make event
         const content = {
             tokens: {
@@ -104,6 +139,7 @@ async function makeEvent(
                 ['t', 'internal-transaction-start'],
                 ['t', 'satsback'],
                 ['e', eTag],
+                ['metadata', 'true', 'nip04', metadataContentEncrypt],
             ],
             content: JSON.stringify(content),
             created_at: Math.round(Date.now() / 1000) + 1,
